d4d779c6139e3d841aedb98a97a79ce608e1e2ad
pedrophfsdev <fernandesphfs@gmail.com>
2021-09-02 22:01:45 -0300

    Projeto Dio.Cadastro Finalizado

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace DIO.Cadastro
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,17 @@
+namespace DIO.Cadastro
+{
+  public enum Genero
+  {
+
+    Acao = 1,
+    Aventura = 2,
+    Drama = 3,
+    Comedia = 4,
+    Tecnologia = 5,
+    Faroeste = 6,
+    Ficcao_Cientifica = 7,
+    Musical = 8,
+    Terror = 9,
+    Suspense = 10
+  }
+}
